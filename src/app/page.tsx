03635ef746b69a5f5807f22ecf5b760b7db0ab62
Ushi54 <regile.hidaka@gmail.com>
2025-06-09 23:47:35 +0900

    カードだけ全て実装、画像は後で実装する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,22 @@
 'use client';
 
-import { Box, cardClasses, Stack } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import ContentsCard from './components/ContentsCard';
 import cards from './data/cards.json';
 
 export default function Home() {
   return (
-    <Box sx={{ p: 4 }}>
-
-      {/* ドラム */}
-<ContentsCard title={cards[0].title} description={cards[0].description} image={cards[0].image} />
+    <Box sx={{ p: 7 }}>
+      <Grid container spacing={4}>
+        {cards.map((card) => (
+          <ContentsCard
+            key={card.id}
+            title={card.title}
+            description={card.description}
+            image={card.image}
+          />
+        ))}
+      </Grid>
     </Box>
   );
 }
