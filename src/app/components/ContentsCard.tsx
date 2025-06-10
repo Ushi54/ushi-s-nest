018f4329deaba3343298b2927644cbe8ef914e73
Ushi54 <regile.hidaka@gmail.com>
2025-06-10 23:29:55 +0900

    💄 リンク追加

--- src/app/components/ContentsCard.tsx
+++ src/app/components/ContentsCard.tsx
@@ -1,36 +1,40 @@
 'use client';
 
 import { Box, Card, CardActionArea, CardContent, CardMedia, Stack, Typography, Grid } from '@mui/material';
+import Link from 'next/link';
 
 interface ContentsCardProps {
   title: string;
   description: string;
   image: string;
+  url: string;
 }
 
-export default function ContentsCard({ title, description, image }: ContentsCardProps) {
+export default function ContentsCard({ title, description, image, url }: ContentsCardProps) {
   return (
     <Grid item xs={6} sm={6}>
-      <Card sx={{ height: '100%' }}>
-        <CardActionArea>
-          <CardMedia
-            component="img"
-            height="100"
-            image={image}
-            alt={title}
-          />
-          <CardContent
-            sx={{ height: '120px' }}
-          >
-            <Typography gutterBottom variant="h6" component="div">
-              {title}
-            </Typography>
-            <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-              {description}
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-      </Card>
+      <Link href={url} style={{ textDecoration: 'none' }}>
+        <Card sx={{ height: '100%' }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="100"
+              image={image}
+              alt={title}
+            />
+            <CardContent
+              sx={{ height: '120px' }}
+            >
+              <Typography gutterBottom variant="h6" component="div">
+                {title}
+              </Typography>
+              <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                {description}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Link>
     </Grid>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,7 @@ export default function Home() {
             title={card.title}
             description={card.description}
             image={card.image}
+            url={card.url}
           />
         ))}
       </Grid>
